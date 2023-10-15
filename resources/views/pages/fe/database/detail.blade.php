7a69c7147d942cecd7d4d17946f28bb15718ae35
hafidr <hafid.riyadi17@gmail.com>
2023-10-15 12:43:12 +0700

    Add breadcrumb on hoya detail page

--- resources/views/pages/fe/database/detail.blade.php
+++ resources/views/pages/fe/database/detail.blade.php
@@ -9,7 +9,23 @@
         </div>
     </div>
 </section>
-<section class="service-details pt_140 pb_140">
+<div class="service-details auto-container my-4">
+    <div class="row">
+        <div class="col-sm-12 col-lg-6">
+            <button type="button" class="btn btn-outline-secondary" onclick="history.back()">Kembali</button>
+        </div>
+        <div class="col-sm-12 col-lg-6">
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-end" style="background-color: transparent">
+                    <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{url('/database')}}">Database Hoya</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{$data->name}}</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+<section class="service-details pb_140">
     <div class="auto-container">
         <div class="row clearfix">
             <div class="col-lg-4 col-md-12 col-sm-12 sidebar-side">
